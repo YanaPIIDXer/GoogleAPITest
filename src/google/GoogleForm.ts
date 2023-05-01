3f2062cab18ae7932f12ceb079785bcad30ba13c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-01 17:05:31 +0900

    フォームの作成と列挙メソッド

--- src/google/GoogleForm.ts
+++ src/google/GoogleForm.ts
@@ -1,4 +1,17 @@
 import axios, { type AxiosInstance } from "axios";
+import { useStore } from "vuex";
+import type { StoreState } from "../stores";
+
+const store = useStore<StoreState>();
+
+/**
+ * フォームの情報
+ */
+export interface FormInfo {
+  title: string;
+  documentTitle?: string;
+  description?: string;
+}
 
 /**
  * Google Formのインスタンス
@@ -8,8 +21,34 @@ export class GoogleForm {
    * 生成
    * @returns 生成されたGoogleFormのインスタンス
    */
-  public static create(): GoogleForm {
-    throw new Error("Not Implemented");
+  public static async create(info: FormInfo): Promise<GoogleForm> {
+    const endpoint = "https://forms.googleapis.com/v1/forms";
+    try {
+      const body = {
+        info,
+        items: [],
+      };
+      const response = await axios.post(endpoint, body);
+      if (Math.floor(response.status / 200) !== 2) {
+        throw response;
+      }
+
+      // Storeに追加してインスタンスを返す
+      const formId: string = response.data.formid;
+      store.commit("addForm", formId);
+      return new GoogleForm(formId);
+    } catch (error) {
+      console.error("Create Form Failed.");
+      throw error;
+    }
+  }
+
+  /**
+   * フォームのリストを取得
+   * @returns フォームのリスト
+   */
+  public static getList(): GoogleForm[] {
+    return store.state.forms.map((f) => new GoogleForm(f.id));
   }
 
   private axios: AxiosInstance;
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,14 +1,24 @@
-import Vuex from "vuex";
+import { createStore } from "vuex";
 import createPersistedState from "vuex-persistedstate";
-import type { Form } from "./states/Forms";
+import type { Form } from "./states/Form";
+
+export { Form };
 
 export interface StoreState {
   forms: Form[];
 }
 
-export default new Vuex.Store<StoreState>({
+export default createStore<StoreState>({
   state: {
     forms: [],
+  },
+  mutations: {
+    /**
+     * フォーム追加
+     */
+    addForm: (state: StoreState, payload: string) => {
+      state.forms.push({ id: payload });
+    },
   },
   plugins: [createPersistedState({ storage: window.localStorage })],
 });
