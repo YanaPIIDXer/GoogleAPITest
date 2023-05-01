a900f50cccddd954b77c2bb6cbdafe3093798946
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-01 17:12:52 +0900

    規模がデカいので断念 多分これは対話型のプロンプトとかの方が向いてる

--- src/views/FormsView.vue
+++ src/views/FormsView.vue
@@ -4,7 +4,12 @@
 <template lang="pug">
 .rootView
   h1 Google Forms API 実験
+  .retire 規模がデカいので撤退。。。
 </template>
 
 <style lang="sass" scoped>
+.rootView
+  .retire
+    font-size: 32px
+    font-weight: bold
 </style>
