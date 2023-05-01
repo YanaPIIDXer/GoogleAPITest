ff29acdd989a7576a4cc94f3c96b5618d154dced
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-01 16:23:06 +0900

    Google APIキーを環境変数から参照できるようにした

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  /**
+   * Google APIキー
+   */
+  readonly VITE_APP_GOOGLE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
