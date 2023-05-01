004e8a90d76ad6f72a1a556322c1007c809f76a8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-01 16:33:22 +0900

    baseURL修正

--- src/google/GoogleForm.ts
+++ src/google/GoogleForm.ts
@@ -19,7 +19,7 @@ export class GoogleForm {
    * @param formId フォームID
    */
   private constructor(private formId: string) {
-    const baseURL = `https://docs.google.com/forms/d/e/${this.formId}`;
+    const baseURL = `https://forms.googleapis.com/v1/forms/${this.formId}`;
     this.axios = axios.create({
       baseURL,
     });
